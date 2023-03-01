5922b54bed7cd014446126b2d16543d036dcec93
pd6974 <pdolan17@gmail.com>
2023-02-28 18:43:13 -0600

    Numbers and operators now appear in the display.  The calculation displays correctly in the console

--- script.js
+++ script.js
@@ -1,44 +1,101 @@
-const container = document.querySelector('.numbers');
+// make number grid
+const numbersDiv = document.querySelector('.numbers');
 makeGrid();
 
 const operators = ['+', '-', '*', '/'];
 
+// add operators to calculator
+const operatorsDiv = document.querySelector('.operators');
+makeOperatorGrid();
 
+// select display and allow numbers to appear
+const displayDiv = document.querySelector('.display');
+
+const container = document.querySelector('.container')
+
+container.addEventListener("click", (e) => {
+    if(e.target.id == "*" || e.target.id == "+" || e.target.id == "-" || e.target.id == "/") {
+        x = logIt(e);
+        ope = e.target.id
+    }
+    else if (e.target.id == "Enter") {
+        console.log(solve(x, ope))
+    }
+    displayNumber(e);
+});
+
+
+function solve(a, op) {
+    b = Number(displayDiv.textContent)
+    a = Number(a)
+    return operate(ope, a, b)
+}
+
+// How can I get the variables in the first if statement to carry into the else if?
+function logIt(e) {
+        return displayDiv.textContent
+}
+
+
+function displayNumber(e) {
+    if (e.target !== e.currentTarget) {
+        if (!isNaN(e.target.id)) {
+            var clickedItem = e.target.id;
+            displayDiv.textContent += clickedItem
+        } else if (e.target.id == "Clear") {
+            displayDiv.textContent = ""
+        }
+        else if (e.target.id == "Enter") {
+            
+        } else {
+            var clickedItem = e.target.id
+            displayDiv.textContent = ""
+        }
+
+    }
+
+}
+
+// logic for creating number grid
 function makeGrid() {
     for (let i = -2; i <= 9; i++) {
             const gridButton = document.createElement('button');
-            container.appendChild(gridButton);
+            numbersDiv.appendChild(gridButton);
             gridButton.classList.add(`button${i}`);
             if (i == -1){
                 gridButton.textContent = "Enter"
+                gridButton.setAttribute('id', "Enter")
             } else if (i == -2) {
                 gridButton.textContent = "Clear"
+                gridButton.setAttribute('id', "Clear")
             } else {
             gridButton.textContent = `${i}`;
+            gridButton.setAttribute('id', `${[i]}`)
             }
     }
 }
 
-const operatorsDiv = document.querySelector('.operators');
-makeOperatorGrid();
-
+// logic for add operators to calculator
 function makeOperatorGrid () {
     for (let i = 0; i < 4; i++) {
         const operatorButton = document.createElement('button');
         operatorsDiv.appendChild(operatorButton);
         operatorButton.classList.add('operatorButton');
+        operatorButton.setAttribute('id', `${operators[i]}`)
         operatorButton.textContent = `${operators[i]}`
     }
 }
 
-
-
-
-
-
-
-
-
+// logic to add click event listeners to calculator >> research how to add event listeners in a loop
+function clickToDisplay () {
+    for (let i = -2; i <= 9; i++) {
+        const numbers = document.querySelector(`.button${i}`);
+        numbers.addEventListener('click', () => {
+            temp = i
+            displayDiv.textContent = temp
+        })
+    }
+}
 
 //simple functions for calculator operations
 
